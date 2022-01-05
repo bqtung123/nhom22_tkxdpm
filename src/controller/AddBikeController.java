5f399d488159b6c69676d04b789cfa3fee2c70b9
Mai Tuan Hung <hungnaruto2511@gmail.com>
2022-01-05 10:32:02 +0700

    update add bike

--- src/controller/AddBikeController.java
+++ src/controller/AddBikeController.java
@@ -12,6 +12,7 @@ public class AddBikeController extends BaseController {
 	public String bikeType;
 	public String bikeLicense;
 	public String bikeProducer;
+	public String bikeCost;
 	public int bikeParkId;
 	
 	public int getBikeParkId() {
@@ -48,25 +49,37 @@ public class AddBikeController extends BaseController {
 		return this.bikeProducer;
 	}
 	
+	public void setBikeCost(String bikeCost){
+		this.bikeCost = bikeCost;
+	}
+	public String getBikeCost() {
+		return this.bikeCost;
+	}
+	
 	//methods
-	public void AddBikeToDB() throws SQLException {
-//		Statement stm = AIMSDB.getConnection().createStatement();
-//        ResultSet res = stm.executeQuery("INSERT INTO Bike (id, COLUMN_2,..) VALUES (VALUE_1,VALUE_2,..)");
-        
-     // the mysql insert statement
-        String query = " insert into Bike (name, type, license, producer,parkId)"
-          + " values (?, ?, ?, ?,?)";
+	public boolean AddBikeToDB() throws SQLException {
+		if(bikeCost == "" || bikeName == "" || bikeLicense == "" || bikeProducer == "" || bikeType == "") {
+			return false;
+		} else {
 
-        // create the mysql insert preparedstatement
-        PreparedStatement preparedStmt = AIMSDB.getConnection().prepareStatement(query);
-        preparedStmt.setString(1, this.getBikeName());
-        preparedStmt.setString(2, this.getBikeType());
-        preparedStmt.setString(3, this.getBikeLicense());
-        preparedStmt.setString(4, this.getBikeProducer());
-        preparedStmt.setInt(5, this.getBikeParkId());
+	        String query = " insert into Bike (name, type, license, producer, cost, parkId)"
+	          + " values (?, ?, ?, ?, ?, ?)";
 
-        // execute the preparedstatement
-        preparedStmt.execute();
+	        // create the mysql insert preparedstatement
+	        PreparedStatement preparedStmt = AIMSDB.getConnection().prepareStatement(query);
+	        preparedStmt.setString(1, this.getBikeName());
+	        preparedStmt.setString(2, this.getBikeType());
+	        preparedStmt.setString(3, this.getBikeLicense());
+	        preparedStmt.setString(4, this.getBikeProducer());
+	        preparedStmt.setInt(5, Integer.parseInt(this.getBikeCost()));
+	        preparedStmt.setInt(6, this.getBikeParkId());
+
+	        // execute the preparedstatement
+	        preparedStmt.execute();
+	        
+	        return true;
+		}
+		
 	}
 	
 //	"id"	INTEGER,
--- src/views/screen/addBike/AddBikeHandle.java
+++ src/views/screen/addBike/AddBikeHandle.java
@@ -1,6 +1,7 @@
 package views.screen.addBike;
 
 import java.io.IOException;
+import utils.Configs;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
@@ -16,13 +17,14 @@ import entity.rent.RentalBike;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import utils.Configs;
 import views.screen.BaseScreenHandler;
 import views.screen.home.HomeScreenHandler;
 import views.screen.invoice.InvoiceScreenHandler;
@@ -42,6 +44,9 @@ public class AddBikeHandle extends BaseScreenHandler implements Initializable{
 	
 	@FXML
 	private TextField addBike_producer;
+	
+	@FXML
+	private TextField addBike_cost;
 	
 	private Bike bike;
 	
@@ -66,32 +71,88 @@ public class AddBikeHandle extends BaseScreenHandler implements Initializable{
 		addBikeController.setBikeType(addBike_type.getText());
 		addBikeController.setBikeLicense(addBike_license.getText());
 		addBikeController.setBikeProducer(addBike_producer.getText());
+		addBikeController.setBikeCost(addBike_cost.getText());
 		addBikeController.setBikeParkId(parkId);
-		addBikeController.AddBikeToDB();
-
-//		Stage window = new Stage();
-//		window.initModality(Modality.APPLICATION_MODAL);
-//		window.setTitle("Notification");
-//		window.setMinWidth(600);
-//		window.setMinHeight(400);
-//		
-//		Button button = new Button();
-//		button.setOnAction(e -> {
-//			HomeScreenHandler homeHandler;
-//			try {
-//				homeHandler = new HomeScreenHandler(this.stage, Configs.HOME_PATH);
-//				homeHandler.setScreenTitle("Home Screen");
-//				homeHandler.show();
-//				window.close();
-//			} catch (IOException e1) {
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
-//		});
 		
-		NotiAdd.Display(this.stage, null,parkId);
+		if(addBikeController.AddBikeToDB()) {
+			ShowSuccessPopup();
+		} else {
+			ShowErrorPopup();
+		}
 	}
 	
+	@FXML
+	public void cancelAddBike(ActionEvent event) throws IOException, SQLException {
+		   
+    	BaseScreenHandler thueXe = new HomeScreenHandler(this.stage, Configs.HOME_PATH, parkId);
+		thueXe.setBController(new RentalBikesController());
+		thueXe.setScreenTitle("Thuê xe");
+		thueXe.show();
+    }
+	
+	private void ShowSuccessPopup() {
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle("Thêm thành công");
+		window.setWidth(400);
+		window.setHeight(200);
+		
+		Button button = new Button("OK");
+		button.setOnAction(e -> {
+			HomeScreenHandler homeHandler;
+			try {
+				homeHandler = new HomeScreenHandler(this.stage, Configs.HOME_PATH,parkId);
+				homeHandler.setScreenTitle("Home Screen");
+				homeHandler.show();
+				window.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
+		
+		button.setAlignment(Pos.BOTTOM_CENTER);
+		VBox layout = new VBox(20);
+		
+		layout.getChildren().add(new javafx.scene.control.Label("Thêm thành công"));
+		layout.getChildren().addAll(button);
+		layout.setAlignment(Pos.CENTER);
+		Scene scene = new Scene(layout);
+		scene.disposePeer();
+		window.setScene(scene);
+		window.showAndWait();
+	}
+	
+	private void ShowErrorPopup() {
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle("Không thành công.");
+		window.setWidth(400);
+		window.setHeight(200);
+		
+		Button button = new Button("OK");
+		button.setOnAction(e -> {
+			window.close();
+		});
+		
+		button.setAlignment(Pos.BOTTOM_CENTER);
+		
+		
+		VBox layout = new VBox(20);
+		
+		layout.getChildren().add(new javafx.scene.control.Label("Không thành công. Kiểm tra lại thông tin nhập vào"));
+		layout.getChildren().addAll(button);
+		
+		  
+        // add label to vbox
 
+	    
+		layout.setAlignment(Pos.CENTER);
+		Scene scene = new Scene(layout);
+		scene.disposePeer();
+		window.setScene(scene);
+		window.showAndWait();
+	}
+	
 }
 
